ed86ab2c753d76605be4c7ac0aca170d271b9526
Will <doggy.huang@gmail.com>
2024-07-12 02:14:36 +0800

    加入 BindRequired 與 Required 的 Model Binding 範例

---
+++ ModelBindingDemo/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingDemo.Models;
+
+namespace ModelBindingDemo.Controllers;
+
+public class StudentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public ActionResult Index(Student data)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Index", new { result = "OK" });
+        }
+
+        return View(data);
+    }
+}
---
+++ ModelBindingDemo/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingDemo.Models;
+
+public class Student
+{
+    [Required]
+    [BindRequired]
+    public required string Name { get; set; }
+
+    [BindRequired]
+    [Range(0, 99)]
+    public int Age { get; set; }
+
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+}
