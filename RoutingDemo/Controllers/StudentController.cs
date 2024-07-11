533742bdc06015c51510763db0b58794fff2b1f7
Will <doggy.huang@gmail.com>
2024-07-12 02:14:12 +0800

    加入 StudentController 自訂路由範例

---
+++ RoutingDemo/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingDemo.Controllers;
+
+[Route("學生")]
+public class StudentController : Controller
+{
+    [HttpGet("")]
+    [HttpGet("歡迎入學")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
