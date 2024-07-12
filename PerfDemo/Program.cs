e9aac1fc4b8abca23f1af09aa1307e0af01c103c
Will <doggy.huang@gmail.com>
2024-07-13 01:21:00 +0800

    加入 Entity Framework Core 實體資料模型

--- PerfDemo/Program.cs
+++ PerfDemo/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PerfDemo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ContosoUniversityContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
