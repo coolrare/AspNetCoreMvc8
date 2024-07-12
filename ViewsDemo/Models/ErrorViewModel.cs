0ff0374f51c1cbeea274c5224c681cd3eead3316
Will <doggy.huang@gmail.com>
2024-07-12 18:59:16 +0800

    加入 ViewsDemo 專案

---
+++ ViewsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
