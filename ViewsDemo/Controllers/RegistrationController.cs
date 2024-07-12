f037121d54eb3ad46fc05a24553d569fbe99aa93
Will <doggy.huang@gmail.com>
2024-07-13 00:41:00 +0800

    加入範本檢視的範例

---
+++ ViewsDemo/Controllers/RegistrationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers;
+
+public class RegistrationController : Controller
+{
+    public IActionResult Index()
+    {
+        var data = new List<RegistrationViewModel>();
+        data.Add(new RegistrationViewModel()
+        {
+            Id = "A001",
+            FirstName = "John",
+            LastName = "Doe",
+            RegisterDate = DateTime.Now
+        });
+
+        return View(data);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(RegistrationViewModel data)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View(data);
+    }
+
+    public ActionResult Details(string id)
+    {
+        ViewData.Model = new RegistrationViewModel()
+        {
+            Id = id,
+            FirstName = "John",
+            LastName = "Doe",
+            RegisterDate = DateTime.Now
+        };
+
+        return View();
+    }
+}
--- ViewsDemo/Models/RegistrationViewModel.cs
+++ ViewsDemo/Models/RegistrationViewModel.cs
@@ -4,11 +4,20 @@ namespace ViewsDemo.Models;
 
 public class RegistrationViewModel
 {
+    [Required(ErrorMessage = "會員編號為必填")]
+    [Display(Name = "會員編號")]
+    public required string Id { get; set; }
+
     [Required(ErrorMessage = "First Name is required.")]
-    [Display(Name = "First Name")]
+    [Display(Name = "名字")]
     public string? FirstName { get; set; }
 
     [Required(ErrorMessage = "Last Name is required.")]
-    [Display(Name = "Last Name")]
+    [Display(Name = "姓氏")]
     public string? LastName { get; set; }
+
+    // 加入註冊日期
+    [Display(Name = "註冊日期")]
+    //[UIHint("DateTime")]
+    public DateTime? RegisterDate { get; set; }
 }
