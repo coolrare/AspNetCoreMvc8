2556c7d5a9d287e14d1c2da89fb3a0091cc2b97d
Will <doggy.huang@gmail.com>
2024-07-13 10:01:40 +0800

    加入 LiveDemo 專案，外加 EF Core

---
+++ LiveDemo/Program.cs
@@ -0,0 +1,29 @@
+using LiveDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ContosoUniversityContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
